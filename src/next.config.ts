6cbd2e72649851b6c0cf2cf7f259badf747887de
Ugochukwu Jonathan <ugochukwujonathan067@gmail.com>
2025-11-24 21:55:54 +0000

    the api key is already added on vercel envronment variable do i need to

--- src/next.config.ts
+++ src/next.config.ts
@@ -38,6 +38,7 @@ const nextConfig: NextConfig = {
     ],
   },
    env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     NEXT_PUBLIC_FIREBASE_VAPID_KEY: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
   }
 };
