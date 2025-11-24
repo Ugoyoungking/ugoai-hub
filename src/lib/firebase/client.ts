223992af31a8117daf63f3af824e7be150cb0b73
Ugochukwu Jonathan <ugochukwujonathan067@gmail.com>
2025-11-24 22:26:32 +0000

    this might be the reason

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -5,17 +5,25 @@ import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDty7nU-OJHvBAmvxdEK8GZaYp_fXVeKeM",
-  authDomain: "textme-76d52.firebaseapp.com",
-  projectId: "textme-76d52",
-  storageBucket: "textme-76d52.appspot.com",
-  messagingSenderId: "1016897162508",
-  appId: "1:1016897162508:web:66b2b777ef9d7a0b274d29",
-  measurementId: "G-GLNQDQRV6V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 function getClientApp(): FirebaseApp {
-  return getApps().length ? getApp() : initializeApp(firebaseConfig);
+  if (getApps().length) {
+    return getApp();
+  }
+  
+  if (!firebaseConfig.apiKey) {
+    throw new Error('Missing Firebase API key from environment variables.');
+  }
+
+  return initializeApp(firebaseConfig);
 }
 
 function getClientAuth(): Auth {
